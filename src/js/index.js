9971637557a1d7ce348a25e37e09661ac3871602
tinkerscript <river.burbot@yandex.ru>
2016-05-06 22:23:30 +0500

    call match before render in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router, browserHistory } from 'react-router'
+import { match, Router, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import configureStore from './store/configureStore'
 import routes from './routes'
 
 const store = configureStore(window.__INITIAL_STATE__);
 const history = syncHistoryWithStore(browserHistory, store);
+const { pathname, search, hash } = window.location
+const location = `${pathname}${search}${hash}`
 
-render(
-  <Provider store={store}>
-    <Router history={history} routes={routes}/>
-  </Provider>,
-  document.getElementById('root')
-)
+match({ routes, location }, () => {
+  render(
+    <Provider store={store}>
+      <Router history={history} routes={routes}/>
+    </Provider>,
+    document.getElementById('root')
+  )
+})
